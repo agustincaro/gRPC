4e7c188c2ce39d01c801187cf5ae68529f803af4
agu <agustin.robuco@gmail.com>
2022-11-19 11:42:46 -0300

    scaffolding 2

---
+++ internal/domain/buyer.go
@@ -0,0 +1,14 @@
+package domain
+
+type Buyer struct {
+	ID           int    `json:"id"`
+	CardNumberID string `json:"card_number_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+}
+
+type BuyerRequest struct {
+	CardNumberID string `json:"card_number_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+}
